fe6f2ec1a555d3b5a068bfd3225ede2f735135c6
Thorsten Eggers <thorsten@eggers-bhv.de>
2025-11-11 09:20:30 +0100

    Class created

---
+++ RealAbilitySystem/Source/RealAbilitySystem/Private/RealGameplayAbility.cpp
@@ -0,0 +1,4 @@
+// Copyright EggersBhv
+
+
+#include "RealGameplayAbility.h"
---
+++ RealAbilitySystem/Source/RealAbilitySystem/Public/RealGameplayAbility.h
@@ -0,0 +1,16 @@
+// Copyright EggersBhv
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "RealGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REALABILITYSYSTEM_API URealGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+};
