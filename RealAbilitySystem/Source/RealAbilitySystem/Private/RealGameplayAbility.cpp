a047169b28706e5fde6edb557744e6c736db9045
Thorsten Eggers <thorsten@eggers-bhv.de>
2025-11-11 09:34:03 +0100

    Functions implemented

--- RealAbilitySystem/Source/RealAbilitySystem/Private/RealGameplayAbility.cpp
+++ RealAbilitySystem/Source/RealAbilitySystem/Private/RealGameplayAbility.cpp
@@ -2,3 +2,52 @@
 
 
 #include "RealGameplayAbility.h"
+#include "RealAbilitySystemComponent.h"
+
+URealAbilitySystemComponent* URealGameplayAbility::GetRealAbilitySystemComponentFromActorInfo() const
+{
+	return (CurrentActorInfo ? Cast<URealAbilitySystemComponent>(CurrentActorInfo->AbilitySystemComponent.Get()) : nullptr);
+}
+
+AController* URealGameplayAbility::GetControllerFromActorInfo() const
+{
+	if (CurrentActorInfo)
+	{
+		if (AController* PC = CurrentActorInfo->PlayerController.Get())
+		{
+			return PC;
+		}
+
+		// Look for a player controller or pawn in the owner chain.
+		AActor* TestActor = CurrentActorInfo->OwnerActor.Get();
+		while (TestActor)
+		{
+			if (AController* C = Cast<AController>(TestActor))
+			{
+				return C;
+			}
+
+			if (APawn* Pawn = Cast<APawn>(TestActor))
+			{
+				return Pawn->GetController();
+			}
+
+			TestActor = TestActor->GetOwner();
+		}
+	}
+
+	return nullptr;
+}
+
+void URealGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (bDrawDebugs && IsValid(GEngine))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 3.f,
+			FColor::Cyan, FString::Printf(TEXT("%s Activated"), *GetName()));
+	}
+}
--- RealAbilitySystem/Source/RealAbilitySystem/Public/RealGameplayAbility.h
+++ RealAbilitySystem/Source/RealAbilitySystem/Public/RealGameplayAbility.h
@@ -6,6 +6,8 @@
 #include "Abilities/GameplayAbility.h"
 #include "RealGameplayAbility.generated.h"
 
+class URealAbilitySystemComponent;
+
 /**
  * 
  */
@@ -13,4 +15,19 @@ UCLASS()
 class REALABILITYSYSTEM_API URealGameplayAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	URealAbilitySystemComponent* GetRealAbilitySystemComponentFromActorInfo() const;
+	UFUNCTION(BlueprintCallable)
+	AController* GetControllerFromActorInfo() const;
+	
+protected:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "RealAbilitySystem|Debug")
+	bool bDrawDebugs = false;
 };
