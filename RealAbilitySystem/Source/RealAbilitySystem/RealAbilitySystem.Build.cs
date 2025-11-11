59ee20625f6e8cb7eb08307cf979787c76cfc333
Thorsten Eggers <thorsten@eggers-bhv.de>
2025-11-11 08:56:35 +0100

    GameplayAbilities dependencies added

--- RealAbilitySystem/Source/RealAbilitySystem/RealAbilitySystem.Build.cs
+++ RealAbilitySystem/Source/RealAbilitySystem/RealAbilitySystem.Build.cs
@@ -38,6 +38,9 @@ public class RealAbilitySystem : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+				"GameplayAbilities",
+				"GameplayTags",
+				"GameplayTasks",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
